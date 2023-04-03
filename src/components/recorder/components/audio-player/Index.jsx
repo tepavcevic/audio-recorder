72321f7469bff090710913c43f2152ce7b1d8571
tepavcevic <djordje.tepa@gmail.com>
2023-04-03 13:39:27 +0200

    Refactoring Audio Player component
    Deleted Audio Recording component and moving its code in tracks.map
    Did so because smaller component only did displaying the passed props

--- src/components/recorder/components/audio-player/Index.jsx
+++ src/components/recorder/components/audio-player/Index.jsx
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 
 import handleAudioRecordings from '../../../../domain/audio-reproduction/handleAudioRecordings';
-import AudioRecording from '../audio-recording/Index';
+import downloadIcon from '../../../../assets/downloadIcon.svg';
 import './styles.css';
 
 export default function AudioPlayer({ audioTracks }) {
@@ -21,12 +21,22 @@ export default function AudioPlayer({ audioTracks }) {
       <h2 className="appHeading">Your Recordings</h2>
 
       {audioTracks.map((track) => (
-        <AudioRecording
-          track={track}
-          key={track.id}
-          ref={addToAudioRecordings}
-          togglePlay={toggleRecordingsPlaying}
-        />
+        <div className="trackGroup" key={track.id}>
+          <audio
+            id={track.id}
+            src={track.audioUrl}
+            ref={addToAudioRecordings}
+            onPlay={() => toggleRecordingsPlaying(track.id)}
+            controls
+            preload="metadata"
+          >
+            Your browser is outdated, please update.
+          </audio>
+
+          <a className="btnDownload" download href={track.audioUrl}>
+            <img id="downloadIcon" src={downloadIcon} alt="Download icon" />
+          </a>
+        </div>
       ))}
     </div>
   );
